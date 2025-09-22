76172b58aaec76b2d70d5edd55e7872dba4f616c
Mushkatin Daniil <mushkatindaniil@gmail.com>
2025-09-22 03:08:28 +0300

    Add files via upload

---
+++ projectInt/src/com/projectInt/projectInt.java
@@ -0,0 +1,11 @@
+package com.projectInt;
+
+public class projectInt {
+    public static void main(String[] args) {
+        int income = 120000;
+        int spending = 54000;
+        int remainder = income-spending;
+        System.out.println("Итого руб:");
+        System.out.println(remainder);
+    }
+}
