0378996fcbf11aaeb00811d7598bab59cacde06a
Mushkatin Daniil <mushkatindaniil@gmail.com>
2025-10-02 00:32:01 +0300

    Add files via upload

---
+++ ProjectWorks/src/fr/tax/Main.java
@@ -0,0 +1,82 @@
+package fr.tax;
+
+import java.util.Scanner;
+
+public class Main {
+    public static double incomeTax(int income) {
+
+        double bestSix = income * 0.06;
+        if (bestSix>0) {
+            return bestSix;
+        }
+        else {
+            return 0;
+        }
+
+
+    }
+
+    public static double incomeExpensesTax(int income, int expenses) {
+        double incExpTax = (income - expenses) * 0.15;
+        if (incExpTax>0) {
+            return incExpTax;
+        }
+        else {
+            return 0;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int incomeSum = 0;
+        int expensesSum = 0;
+
+        while (true) {
+            System.out.println("Выберите операцию и введите ее номер:\n 1. Добавить новый доход\n " +
+                    "2. Добавить новый расход\n 3.Выбрать систему налогооблажения");
+
+            int number = s.nextInt();
+
+            if (number == 1) {
+                while (true) {
+                    System.out.println("Введите доходы!:");
+                    int income = s.nextInt();
+                    incomeSum = incomeSum + income;
+                    System.out.println(incomeSum);
+                    break;
+                }
+            } else if (number == 2) {
+                while (true) {
+                    System.out.println("Введите раходы!:");
+                    int expenses = s.nextInt();
+                    expensesSum = expensesSum + expenses;
+                    System.out.println(expensesSum);
+                    break;
+                }
+            } else if (number == 3) {
+                double firstTaxx = incomeTax(incomeSum);
+                double secondTaxx = incomeExpensesTax(incomeSum, expensesSum);
+                if (firstTaxx > secondTaxx) {
+                    System.out.println("Мы советуем вам УСН доходы минус расходы.");
+                    System.out.println("Ваш налог составит: " + secondTaxx);
+                    System.out.println("Налог на другой системе составит" + firstTaxx);
+                    System.out.println("Экономия: " + (firstTaxx - secondTaxx));
+                    break;
+                } else if (firstTaxx < secondTaxx) {
+                    System.out.println("Мы советуем вам УСН доходы");
+                    System.out.println("Ваш налог составит: " + firstTaxx);
+                    System.out.println("Налог на другой системе составит " + secondTaxx);
+                    System.out.println("Экономия: " + (secondTaxx - firstTaxx));
+                    break;
+
+                } else {
+                    System.out.println("Вы можете выбрать любую систему налогообложения");
+                    break;
+
+                }
+
+            }
+        }
+    }
+}
---
+++ ProjectWorks/src/hw/tax/Main2.java
@@ -0,0 +1,85 @@
+package hw.tax;
+import java.util.Scanner;
+public class Main2 {
+
+    public static int incomeValueFirst (int income) {
+    int incTax = (income/100) * 6;
+    if (incTax >= 0) {
+        return incTax;
+    }
+    else {
+        return 0;
+    }
+    }
+    public static int incomeValueSecond (int income, int expenses) {
+       int incExpTax = (income - expenses) * 15/100;
+       if (incExpTax >= 0) {
+           return incExpTax;
+       }
+       else {
+           return 0;
+
+       }
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int income = 0;
+        int expenses = 0;
+        while (true) {
+            System.out.println("Выберите операцию и введите ее номер:\n 1. Добавить новый доход\n " +
+                    "2. Добавить новый расход\n 3.Выбрать систему налогооблажения");
+            String input = s.nextLine();
+            if (input.equals("end")) {
+                System.out.println("Программа завершена!");
+            break;
+        }
+
+            int number = Integer.parseInt(input);
+            switch(number) {
+                case 1:
+                    System.out.println("Введите сумму доходов:");
+                    String incomeStr = s.nextLine();
+                    int userIncome = Integer.parseInt(incomeStr);
+                    income += userIncome;
+                    System.out.println("Сумма доходов: " + income);
+                    break;
+
+                case 2:
+                    System.out.println("Введите сумму расходов:");
+                    String expensesStr = s.nextLine();
+                    int userExpenses = Integer.parseInt(expensesStr);
+                    expenses += userExpenses;
+                    System.out.println("Сумма доходов: " + expenses);
+                    break;
+
+                case 3:
+                   int firstTax = incomeValueFirst(income);
+                   int secondTax = incomeValueSecond(income, expenses);
+                    if (firstTax > secondTax) {
+                        System.out.println("Мы советуем вам УСН доходы минус расходы.");
+                        System.out.println("Ваш налог составит: " + secondTax);
+                        System.out.println("Налог на другой системе составит" + firstTax);
+                        System.out.println("Экономия: " + (firstTax - secondTax));
+                        break;
+                    }
+                    else if (firstTax < secondTax) {
+                        System.out.println("Мы советуем вам УСН доходы");
+                        System.out.println("Ваш налог составит: " + firstTax);
+                        System.out.println("Налог на другой системе составит " + secondTax);
+                        System.out.println("Экономия: " + (secondTax - firstTax));
+                        break;
+
+                    }
+                    else {
+                        System.out.println("Вы можете выбрать любую систему налогообложения");
+                        break;
+                    }
+                default:
+                    System.out.println("Такой операции не существует.");
+
+                    }
+
+            }
+            }
+        }
+
