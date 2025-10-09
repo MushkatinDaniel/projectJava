a27a5848f59783773bf264fb0d818cffb466ef52
Mushkatin Daniil <mushkatindaniil@gmail.com>
2025-10-09 17:07:16 +0300

    Update Book.java

--- BookProject/src/ru/book/Book.java
+++ BookProject/src/ru/book/Book.java
@@ -18,20 +18,11 @@ public class Book {
     }
 
     public boolean isBig() {
-        if (pages > 500) {
-            return true;
-        } else {
-            return false;
-        }
+        return pages > 500;
     }
 
     public boolean matches(String word) {
-        if (title.contains(word) || author.contains(word) || Author.surname.contains(word)) {
-            return true;
-        } else {
-            return false;
-        }
-
+        return title.contains(word) || author.contains(word) || Author.surname.contains(word);
     }
 
     public int estimatePrice() {
